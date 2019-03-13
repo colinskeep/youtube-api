b510e466c38b56535606760d7ca83cbd473b4fab
colinskeep <colinjinskeep@gmail.com>
2019-03-13 00:56:39 -0700

    various updates

--- src/v1/controllers/example.js
+++ src/v1/controllers/example.js
@@ -18,41 +18,47 @@ async function youtube(req, res) {
     for (let z = 0; z < arr2.length; z++) {
       const browser = await puppeteer.launch({
         headless: headless,
-        'args': ['--incognito'],
+        //'args': ['--incognito'],
       });
-      const context = await browser.createIncognitoBrowserContext();
-      const page = await context.newPage();
+      //const context = await browser.createIncognitoBrowserContext();
+      const page = await browser.newPage();
+      await page.waitFor(time);
       await page.goto('https://accounts.google.com/signin/v2/identifier?hl=en&continue=https%3A%2F%2Fwww.google.com%2F&flowName=GlifWebSignIn&flowEntry=AddSession');
-      await page.type('.whsOnd.zHQkBf', arr2[z].email);
+      await page.waitFor(time);
+      await page.type('.whsOnd.zHQkBf', arr2[z].email.replace(' ', ''));
       await page.click('div[id="identifierNext"]');
       await page.waitFor(time);
-      await page.type('.whsOnd.zHQkBf', arr2[z].password);
+      await page.type('.whsOnd.zHQkBf', arr2[z].password.replace(' '), '');
       await page.click('div[id="passwordNext"]');
       await page.waitFor(time);
       if (page.url().indexOf('https://accounts.google.com/signin/v2/sl/pwd') > -1 && page.evaluate(() => document.querySelector('#view_container > div > div > div.pwWryf.bxPAYd > div > div.WEQkZc > div > form > content > section > div > content > div.Xk3mYe.VxoKGd.Jj6Lae > div.xgOPLd > div:nth-child(2) > content').innerText == 'Wrong password. Try again or click Forgot password to reset it.')) {
         console.log('invalid password for email: ', arr2[z].email);
         continue;
       }
+      console.log(page.url().indexOf('https://accounts.google.com/signin/v2/challenge/'));
+      console.log(page.url());
       if (page.url().indexOf('https://accounts.google.com/signin/v2/challenge/') > -1) {
         console.log('verification needed');
         if (await page.$('div[class="vdE7Oc"]') !== null) {
           await page.click('div[class="vdE7Oc"]');
           await page.waitFor(time);
           await page.type('.whsOnd.zHQkBf', process.env.USER_RECOVERY_EMAIL);
+          await page.waitFor(time);
           if (await page.$('div[class="ZFr60d CeoRYc"]') !== null) {
             await page.click('div[class="ZFr60d CeoRYc"]');
-            await page.waitFor(time);
           }
+          await page.waitFor(time);
           if (await page.$('div[class="ZFr60d CeoRYc"]') !== null) {
             await page.click('div[class="ZFr60d CeoRYc"]');
-            await page.waitFor(time);
           }
+          await page.waitFor(time);
           if (page.url().indexOf('https://accounts.google.com/signin/v2/challenge/') > -1) {
             console.log('unable to authenticate: ', arr[z].email, 'with recovery email');
             continue;
           }
         }
       }
+      await page.waitFor(time);
       console.log('logged in: ', arr2[z].email);
       await page.goto('https://www.youtube.com/channel_switcher');
       console.log('checking brand accounts.....');
@@ -64,7 +70,6 @@ async function youtube(req, res) {
       }
       await page.waitFor(time);
       const hrefs = await page.$$eval('a', as => as.map(a => a.href));
-      console.log(hrefs);
       let arr = [];
       arr.push('https://www.youtube.com/signin?feature=channel_switcher&next=%2F&authuser=0&action_handle_signin=true&skip_identity_prompt=True');
       for (let i = 0; i < hrefs.length; i++) {
